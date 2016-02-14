2f17b447c4527404e15182ff63fa44cdab959b21
Saswat Padhi <saswat.sourav@gmail.com>
2016-02-13 16:36:25 -0800

    Separate script to create a cgroup; changed check_all script name

--- ClangTool/setup.sh
+++ ClangTool/setup.sh
@@ -12,14 +12,6 @@ LLVM_ROOT="`cd \"$1\" && pwd`"
 ABDUCER_ROOT="`cd ../abducer/ && pwd`"
 WORKING_ROOT="`cd ../logs/ || mkdir -p ../logs/ && pwd`"
 
-CGROUP_NAME="$2"
-if [[ "$CGROUP_NAME" != "" ]]; then
-    echo "[+] Creating cgroup [$CGROUP_NAME]."
-    sudo cgcreate -t $USER:$USER -a $USER:$USER -g memory,cpu:$CGROUP_NAME
-    cgset -r memory.limit_in_bytes=$((8*1024*1024*1024)) $CGROUP_NAME
-    cgset -r memory.memsw.limit_in_bytes=$((8*1024*1024*1024)) $CGROUP_NAME
-fi
-
 cd "$LLVM_ROOT/tools/clang/tools"
 
 NEW_TARGET_LINE="add_subdirectory(pinvgen)"
@@ -34,7 +26,7 @@ perl -pe 's#__WORKING_PATH_BASE_FROM_SETUP_SCRIPT__#'"$WORKING_ROOT"'#g' < "$OWN
 perl -pi -e 's#__ABDUCER_PATH_FROM_SETUP_SCRIPT__#'"$ABDUCER_ROOT"'#g' checker
 chmod +x checker
 
-perl -pe 's#__ABDUCER_PATH_FROM_SETUP_SCRIPT__#'"$ABDUCER_ROOT"'#g' < "$OWN_ROOT/check_all.template.sh" > check_all
-chmod +x check_all
+perl -pe 's#__ABDUCER_PATH_FROM_SETUP_SCRIPT__#'"$ABDUCER_ROOT"'#g' < "$OWN_ROOT/check_all.template.sh" > check_all_with_config
+chmod +x check_all_with_config
 
 cmake .. && make -j4 pinvgen
